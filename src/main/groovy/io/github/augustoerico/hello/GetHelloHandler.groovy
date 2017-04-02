9339646721de6b5ce772ca8afeee30bd4ee568aa
Erico Silva <augusto.ericosilva@gmail.com>
2017-04-02 00:08:02 -0300

    Add name on response

--- src/main/groovy/io/github/augustoerico/hello/GetHelloHandler.groovy
+++ src/main/groovy/io/github/augustoerico/hello/GetHelloHandler.groovy
@@ -1,14 +1,16 @@
 package io.github.augustoerico.hello
 
+import io.vertx.ext.auth.jwt.impl.JWTUser
 import io.vertx.ext.web.RoutingContext
 
 class GetHelloHandler {
 
-    static HELLO_JSON = '{"greeting": "Hello!"}'
-
     static handle = { RoutingContext context ->
+        JWTUser user = context.user()
+        def principal = user.jwtToken.map // TODO: REPORT THIS BUG! jwtToken != principal()
+        def sub = principal.sub
         def response = context.response()
-        response.putHeader('content-type', 'application/json').end(HELLO_JSON)
+        response.putHeader('content-type', 'application/json').end("{\"greeting\": \"Hello, $sub!\"}")
     }
 
 }
